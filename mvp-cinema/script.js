3445cf8478386447908d221d63385e4d0dd0f3f9
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-08-29 23:21:20 -0300

    feat: login and authentication

---
+++ mvp-cinema/script.js
@@ -0,0 +1,14 @@
+function login() {
+    let emailEl = document.getElementById("email");
+    let passwordEl = document.getElementById("password");
+
+    let password = passwordEl.value;
+    let email = emailEl.value;
+
+    if (email == "root@admin.com" && password == "123456") {
+        window.location.href = "sistema.html";
+    } else {
+        alert("Usuário e/ou senha não correspondem aos nossos registros!");
+        document.getElementById("loginForm").reset();
+    }
+}
